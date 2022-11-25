e8defcedde9801c8fad1555934bc389aba4d57d0
Bryan Caro <bryancaro96@gmail.com>
2022-11-25 09:53:32 +0100

    Initial Commit

---
+++ Workshop-Template/Workshop_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop_TemplateApp.swift
+//  Workshop-Template
+//
+//  Created by Bryan Caro on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Workshop_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
